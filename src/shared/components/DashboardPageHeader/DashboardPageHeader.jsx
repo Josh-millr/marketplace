94e1a48b897befb15763ce6d51c91a311f7840a4
Valentina <oorhmo@gmail.com>
2023-05-27 18:33:41 +0100

    Feature/Profile

--- src/shared/components/DashboardPageHeader/DashboardPageHeader.jsx
+++ src/shared/components/DashboardPageHeader/DashboardPageHeader.jsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { Box, Grid, Text, Title, Stack, Button } from '@mantine/core';
-=======
-import { Grid, Text, Title, Stack, Button, Flex } from '@mantine/core';
->>>>>>> 26474cec3571449b32feba98cefa71fb45494a17
 
 import { useStyles } from './style.DashboardPageHeader';
 
@@ -25,15 +21,9 @@ export function DashboardPageHeader(props) {
   const { title, descr, action, showButton, btnLabel } = props;
 
   return (
-<<<<<<< HEAD
-    <Box className={classes.wrapper}>
-      <Grid justify="space-between" gutter="lg">
-        <Grid.Col span="auto">
-=======
     <div className={classes.wrapper}>
       <Grid justify="space-between" gutter="4xl">
         <Grid.Col span={12} sm="content">
->>>>>>> 26474cec3571449b32feba98cefa71fb45494a17
           <Stack spacing={0}>
             <Title className="title-lg">{title}</Title>
             <Text className="subtitle-sm" color="neutral.6" maw={480}>
