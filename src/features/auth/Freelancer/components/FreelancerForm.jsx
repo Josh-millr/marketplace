a2fbba9e85a5fa9f1aa27dd61e0123c20dd67593
josh-millr <80153426+Josh-millr@users.noreply.github.com>
2023-05-31 14:08:42 +0000

    Update FreelancerForm.jsx and ProgressStepper.jsx

--- src/features/auth/Freelancer/components/FreelancerForm.jsx
+++ src/features/auth/Freelancer/components/FreelancerForm.jsx
@@ -1,9 +1,10 @@
+import { FreelancerRegistrationStepper } from '@/shared/components/FreelancerRegistrationStepper';
+
 import { PersonalInformationForm } from './PersonalInformationForm';
 import { ProfessionalInformationForm } from './ProfessionalInformationForm';
 import { ExperienceInformationForm } from './ExperienceInformationForm';
 import { ContactInformationForm } from './ContactInformationForm';
 import { OnboardingSuccess } from './OnboardingSuccess';
-import { FreelancerRegistrationStepper } from '@/components/FreelancerRegistrationStepper';
 
 const components = [
   PersonalInformationForm,
--- src/shared/components/FreelancerRegistrationStepper/ProgressStepper/ProgressStepper.jsx
+++ src/shared/components/FreelancerRegistrationStepper/ProgressStepper/ProgressStepper.jsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
-import { Stack, useMantineTheme, Progress } from "@mantine/core";
+import { useState } from 'react';
+import { Stack, useMantineTheme, Progress, Text } from '@mantine/core';
 
 /**
  *
  * TODO: Add horizontal padding to section
  * TODO: pass the increment and decrement functions to the parent component.
  *
- **/
+ * */
 
 const PROGRESS_INCREMENT = 20;
 const MAX_PROGRESS_INCREMENT = 100;
@@ -32,10 +32,10 @@ export const ProgressStepper = ({ incrementProgress, decrementProgress }) => {
   return (
     <div style={{ borderBottom: `1px solid ${colors.neutral[3]}` }}>
       <Stack spacing="md" py="xl">
-        <Text className="label-md" fw={"500!important"}>
+        <Text className="label-md" fw={'500!important'}>
           `Current Progress - (${currProgress})%`
         </Text>
-        <Progress striped animate value={50} radius="xl" value={currProgress} />
+        <Progress value={currProgress} radius="xl" striped animate />
       </Stack>
     </div>
   );
