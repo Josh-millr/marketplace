5501a17aa3c5956dc074db3a9f8de28a1b402730
Josh Millr <80153426+Josh-millr@users.noreply.github.com>
2023-05-31 02:25:22 -1200

    Added page padding

--- src/shared/components/FreelancerRegistrationStepper/BarStepper/BarStepper.jsx
+++ src/shared/components/FreelancerRegistrationStepper/BarStepper/BarStepper.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Stepper } from "@mantine/core";
+import { Stepper, useMantineTheme } from "@mantine/core";
 import {
   IconNumber1,
   IconNumber2,
@@ -7,15 +7,18 @@ import {
   IconNumber4,
 } from "@tabler/icons-react";
 
+import { PageContainer } from "@/shared/components/PageContainer";
+
 /***
  *
- * TODO: Pass in component for each step
  * TODO: Return the `progressStep` function for traversing the steps
  *
  **/
 
 export function BarStepper({ components }) {
   const [active, setActive] = useState(1);
+
+  const { colors } = useMantineTheme();
 
   const StepComponentOne = components[0];
   const StepComponentTwo = components[1];
@@ -33,50 +36,56 @@ export function BarStepper({ components }) {
   };
 
   return (
-    <Stepper active={active} onStepClick={setActive} breakpoint="sm">
-      <Stepper.Step
-        label="Personal Info"
-        completedIcon={<IconNumber1 size="1.1rem" />}
-      >
-        <StepComponentOne
-          goPrevStep={stepControls.prev}
-          goNextStep={stepControls.next}
-        />
-      </Stepper.Step>
+    <div style={{ borderBottom: `1px solid ${colors.neutral[4]}` }}>
+      <PageContainer layout="marketplace">
+        <PageContainer.Marketplace>
+          <Stepper active={active} onStepClick={setActive} breakpoint="sm">
+            <Stepper.Step
+              label="Personal Info"
+              completedIcon={<IconNumber1 size="1.1rem" />}
+            >
+              <StepComponentOne
+                goPrevStep={stepControls.prev}
+                goNextStep={stepControls.next}
+              />
+            </Stepper.Step>
 
-      <Stepper.Step
-        label="Professional Info"
-        completedIcon={<IconNumber2 size="1.1rem" />}
-      >
-        <StepComponentTwo
-          goPrevStep={stepControls.prev}
-          goNextStep={stepControls.next}
-        />
-      </Stepper.Step>
+            <Stepper.Step
+              label="Professional Info"
+              completedIcon={<IconNumber2 size="1.1rem" />}
+            >
+              <StepComponentTwo
+                goPrevStep={stepControls.prev}
+                goNextStep={stepControls.next}
+              />
+            </Stepper.Step>
 
-      <Stepper.Step
-        label="Experience Info"
-        completedIcon={<IconNumber3 size="1.1rem" />}
-      >
-        <StepComponentThree
-          goPrevStep={stepControls.prev}
-          goNextStep={stepControls.next}
-        />
-      </Stepper.Step>
+            <Stepper.Step
+              label="Experience Info"
+              completedIcon={<IconNumber3 size="1.1rem" />}
+            >
+              <StepComponentThree
+                goPrevStep={stepControls.prev}
+                goNextStep={stepControls.next}
+              />
+            </Stepper.Step>
 
-      <Stepper.Step
-        label="Contact Info"
-        completedIcon={<IconNumber4 size="1.1rem" />}
-      >
-        <StepComponentFour
-          goPrevStep={stepControls.prev}
-          goNextStep={stepControls.next}
-        />
-      </Stepper.Step>
+            <Stepper.Step
+              label="Contact Info"
+              completedIcon={<IconNumber4 size="1.1rem" />}
+            >
+              <StepComponentFour
+                goPrevStep={stepControls.prev}
+                goNextStep={stepControls.next}
+              />
+            </Stepper.Step>
 
-      <Stepper.Completed>
-        <StepComponentFive />
-      </Stepper.Completed>
-    </Stepper>
+            <Stepper.Completed>
+              <StepComponentFive />
+            </Stepper.Completed>
+          </Stepper>
+        </PageContainer.Marketplace>
+      </PageContainer>
+    </div>
   );
 }
