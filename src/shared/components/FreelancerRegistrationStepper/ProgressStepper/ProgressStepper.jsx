b44aa361e9e1f1bec67996d5828a432c2a960f73
Josh Millr <80153426+Josh-millr@users.noreply.github.com>
2023-05-31 11:52:44 -1200

    Added componnet step for mobile screen

--- src/shared/components/FreelancerRegistrationStepper/ProgressStepper/ProgressStepper.jsx
+++ src/shared/components/FreelancerRegistrationStepper/ProgressStepper/ProgressStepper.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { Stack, useMantineTheme, Progress, Text } from "@mantine/core";
 
+import withStepper from "@/shared/hocs/withStepper";
+import FormDataProvider from "@/shared/providers/FormDataProvider";
 import { PageContainer } from "@/shared/components/PageContainer";
 
 /**
@@ -12,10 +14,14 @@ import { PageContainer } from "@/shared/components/PageContainer";
 const PROGRESS_INCREMENT = 20;
 const MAX_PROGRESS_INCREMENT = 100;
 
-export const ProgressStepper = ({ incrementProgress, decrementProgress }) => {
+export const ProgressStepper = (props) => {
   const [currProgress, setCurrProgress] = useState(PROGRESS_INCREMENT);
 
+  const { incrementProgress, decrementProgress, components } = props;
+
   const { colors } = useMantineTheme();
+
+  const FormSteps = withStepper(components);
 
   const progressStep = {
     decrement() {
@@ -34,12 +40,18 @@ export const ProgressStepper = ({ incrementProgress, decrementProgress }) => {
     <div style={{ borderBottom: `1px solid ${colors.neutral[3]}` }}>
       <PageContainer layout="marketplace">
         <PageContainer.Marketplace>
-          <Stack spacing="md" py="xl">
-            <Text className="label-md" fw={"500!important"}>
-              Current Progress - {currProgress}%
-            </Text>
-            <Progress value={currProgress} radius="xl" striped animate />
-          </Stack>
+          <>
+            <Stack spacing="md" py="xl">
+              <Text className="label-md" fw={"500!important"}>
+                Current Progress - {currProgress}%
+              </Text>
+              <Progress value={currProgress} radius="xl" striped animate />
+            </Stack>
+
+            <FormDataProvider>
+              <FormSteps />
+            </FormDataProvider>
+          </>
         </PageContainer.Marketplace>
       </PageContainer>
     </div>
