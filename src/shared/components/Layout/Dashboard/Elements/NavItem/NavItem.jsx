64b3e806880cdebe6d6b18038466195268448f2d
Josh Millr <80153426+Josh-millr@users.noreply.github.com>
2023-05-30 13:35:25 -1200

    fixed

--- src/shared/components/Layout/Dashboard/Elements/NavItem/NavItem.jsx
+++ src/shared/components/Layout/Dashboard/Elements/NavItem/NavItem.jsx
@@ -1,10 +1,10 @@
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { Box, NavLink } from '@mantine/core';
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { Box, NavLink } from "@mantine/core";
 
-import { iconCreator } from '@/shared/utils/iconCreator';
+import { iconCreator } from "@/shared/utils/iconCreator";
 
-import { useStyles } from './style.NavItem';
+import { useStyles } from "./style.NavItem";
 
 export function NavItem({ icon: Icon, title, link }) {
   const { classes, theme } = useStyles();
@@ -13,18 +13,20 @@ export function NavItem({ icon: Icon, title, link }) {
   const { pathname } = router;
 
   // Other routes will pass this condition except the dashboard homepage
-  const uniquePath = pathname.split('/').slice(3);
+  const uniquePath = pathname.split("/").slice(3);
   const uniquePathactive = uniquePath.some((path) => link.includes(path));
 
   // Specifically for the dashboard homepage route
-  const isHomeActive = pathname.split('/').length === 0;
+  const isHomeActive = pathname.split("/").length === 0;
 
   const isRouteActive = uniquePathactive || isHomeActive;
-  
+
   const compIcon = iconCreator({
     icon: Icon,
     sizeOverride: 20,
-    colorOverride: active ? theme.colors.neutral[1] : theme.colors.neutral[7],
+    colorOverride: isRouteActive
+      ? theme.colors.neutral[1]
+      : theme.colors.neutral[7],
   });
 
   return (
