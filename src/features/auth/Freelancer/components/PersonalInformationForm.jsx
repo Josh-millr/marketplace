45ba18eeb70fcf9c908c6adb65dde852cc80b74b
Josh Millr <80153426+Josh-millr@users.noreply.github.com>
2023-05-31 12:57:29 -1200

    Refactored profile uload

--- src/features/auth/Freelancer/components/PersonalInformationForm.jsx
+++ src/features/auth/Freelancer/components/PersonalInformationForm.jsx
@@ -3,6 +3,7 @@ import {
   Paper,
   Grid,
   Stack,
+  Flex,
   TextInput,
   Select,
   SimpleGrid,
@@ -26,6 +27,7 @@ export function PersonalInformationForm() {
   const [languages, setLanguages] = useState([]);
   const [profilePicture, setProfilePicture] = useState(null);
 
+  // This function is called by the upload button
   const handlePictureUpload = (e) => {
     const file = e.target.files[0];
     const reader = new FileReader();
@@ -53,12 +55,128 @@ export function PersonalInformationForm() {
   //   setLanguages(updatedLanguages);
   // };
 
+  <Paper>
+    <Divider style={{ margin: "1rem 0" }} />
+
+    <SimpleGrid
+      cols={1}
+      spacing="xl"
+      mt={{ md: 50 }}
+      // breakpoints={[{ minWidth: 'md', cols: 2 }]}
+    >
+      <SimpleGrid cols={1} breakpoints={[{ minWidth: "md", cols: 2 }]}>
+        <TextInput
+          label="First Name"
+          required
+          value={user.firstname}
+          style={{ marginBottom: "1rem" }}
+        />
+        <TextInput
+          label="Last Name"
+          required
+          value={user.lastname}
+          style={{ marginBottom: "1rem" }}
+        />
+      </SimpleGrid>
+
+      <Divider style={{ margin: "1rem 0" }} />
+
+      <SimpleGrid cols={1} breakpoints={[{ minWidth: "md", cols: 2 }]}>
+        <Select
+          label="Gender"
+          placeholder="choose gender"
+          data={[
+            { value: "male", label: "Male" },
+            { value: "female", label: "Female" },
+            { value: "other", label: "Other" },
+          ]}
+          value={user.gender}
+          style={{ marginBottom: "1rem" }}
+        />
+        <Select
+          label="Account Type"
+          placeholder="Select account type"
+          data={[
+            { value: "individual", label: "Individual" },
+            { value: "business", label: "Business" },
+          ]}
+          value={user.accounttype}
+          style={{ marginBottom: "1rem" }}
+        />
+      </SimpleGrid>
+
+      <Divider style={{ margin: "1rem 0" }} />
+
+      <SimpleGrid cols={1} breakpoints={[{ minWidth: "md", cols: 2 }]}>
+        <TextInput
+          type="email"
+          label="Email"
+          required
+          value={user.email}
+          style={{ marginBottom: "1rem" }}
+        />
+        <TextInput
+          label="Country"
+          required
+          value={user.location}
+          style={{ marginBottom: "1rem" }}
+        />
+      </SimpleGrid>
+
+      <Divider style={{ margin: "1rem 0" }} />
+
+      <SimpleGrid cols={1} breakpoints={[{ minWidth: "md", cols: 2 }]}>
+        <TextInput
+          label="Username"
+          description=" Use 3 to 15 characters, letters, numbers, and underscores only,
+    eg. ola_mike."
+          required
+          value={user.username}
+          style={{ marginBottom: "1rem" }}
+        />
+        <TextInput
+          label="Country"
+          description="We’re currently rolling out a few countries, visit our country roll-out plan for more into"
+          required
+          value={user.location}
+          style={{ marginBottom: "1rem" }}
+        />
+      </SimpleGrid>
+
+      <Divider style={{ margin: "1rem 0" }} />
+    </SimpleGrid>
+
+    <SimpleGrid cols={1}>
+      <Select
+        searchable
+        multiple
+        label="Select languages"
+        description="Use 3 to 15 characters, letters, numbers, and underscores only, eg. ola_mike"
+        placeholder="Select languages"
+        data={[
+          { value: "english", label: "English" },
+          { value: "spanish", label: "Spanish" },
+          { value: "french", label: "French" },
+          { value: "german", label: "German" },
+          { value: "italian", label: "Italian" },
+          { value: "japanese", label: "Japanese" },
+        ]}
+        value={languages}
+        onChange={handleLanguageChange}
+        filter={(value, item) =>
+          item.label.toLowerCase().includes(value.toLowerCase().trim())
+        }
+      />
+    </SimpleGrid>
+    <Divider style={{ margin: "1rem 0" }} />
+  </Paper>;
+
   return (
     <PageContainer layout="marketplace">
       <PageContainer.Marketplace>
         <Grid>
-          <Grid.Col span={12} md={7} orderMd={2}></Grid.Col>
-          <Grid.Col span={12} md={5} orderMd={1}>
+          {/* Column 1 */}
+          <Grid.Col span={12} md={5} orderMd={2}>
             <div className={classes.pageHeadingWrapper}>
               <Stack spacing="lg" py="2xl">
                 <Text className="h1">Personal Information</Text>
@@ -70,187 +188,39 @@ export function PersonalInformationForm() {
               </Stack>
             </div>
           </Grid.Col>
+
+          {/* Column 2 */}
+          <Grid.Col span={12} md={7} orderMd={1}>
+            <Grid>
+              <Grid.Col span={12} orderSm={2} sm="content">
+                <Flex justify={{ base: "flex-start", sm: "flex-end" }} w="100%">
+                  <Avatar
+                    size={126}
+                    radius={9999}
+                    variant="filled"
+                    alt="Profile Picture"
+                    src={profilePicture || ""}
+                  />
+                </Flex>
+              </Grid.Col>
+
+              <Grid.Col span={12} orderSm={1} sm="auto">
+                <Stack spacing="lg">
+                  <Stack spacing="xs">
+                    <Text className="label-lg" fw={"500!important"}>
+                      Profile Picture
+                    </Text>
+                    <Text className="body-sm" fw={"500!important"}>
+                      Add a profile picture of yourself so customers will know
+                      exactly who they’ll be working with.
+                    </Text>
+                  </Stack>
+                  <Button leftIcon={<IconPhotoPlus />}>Upload Photo</Button>
+                </Stack>
+              </Grid.Col>
+            </Grid>
+          </Grid.Col>
         </Grid>
-        <Paper>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              marginTop: "2rem",
-              marginBottom: "1rem",
-            }}
-          >
-            <div>
-              <Text
-                variant="h1"
-                size="lg"
-                style={{ textAlign: "left", marginBottom: "1rem" }}
-              >
-                Profile Picture
-              </Text>
-              <Text
-                variant="h2"
-                size="md"
-                style={{ textAlign: "left", marginBottom: "1rem" }}
-              >
-                Add a profile picture of yourself so customers will know exactly
-                who they’ll be working with.
-              </Text>
-              <Button
-                variant="filled"
-                component="label"
-                leftIcon={<IconPhotoPlus />}
-                style={{
-                  marginRight: "1rem",
-                  backgroundColor: "rgba(76, 175, 80, 0.5)",
-                  color: "green",
-                }}
-              >
-                Upload Photo
-                <input
-                  type="file"
-                  accept="image/*"
-                  onChange={handlePictureUpload}
-                  style={{ display: "none" }}
-                />
-              </Button>
-            </div>
-            {profilePicture ? (
-              <Avatar
-                size={180}
-                radius={180}
-                mx="auto"
-                src={profilePicture}
-                alt="Profile Picture"
-              />
-            ) : (
-              <div
-                style={{
-                  width: "180px",
-                  height: "180px",
-                  borderRadius: "50%",
-                  backgroundColor: "gray",
-                  marginRight: "1rem",
-                  marginLeft: "7rem",
-                }}
-              />
-            )}
-          </div>
-
-          <Divider style={{ margin: "1rem 0" }} />
-
-          <SimpleGrid
-            cols={1}
-            spacing="xl"
-            mt={{ md: 50 }}
-            // breakpoints={[{ minWidth: 'md', cols: 2 }]}
-          >
-            <SimpleGrid cols={1} breakpoints={[{ minWidth: "md", cols: 2 }]}>
-              <TextInput
-                label="First Name"
-                required
-                value={user.firstname}
-                style={{ marginBottom: "1rem" }}
-              />
-              <TextInput
-                label="Last Name"
-                required
-                value={user.lastname}
-                style={{ marginBottom: "1rem" }}
-              />
-            </SimpleGrid>
-
-            <Divider style={{ margin: "1rem 0" }} />
-
-            <SimpleGrid cols={1} breakpoints={[{ minWidth: "md", cols: 2 }]}>
-              <Select
-                label="Gender"
-                placeholder="choose gender"
-                data={[
-                  { value: "male", label: "Male" },
-                  { value: "female", label: "Female" },
-                  { value: "other", label: "Other" },
-                ]}
-                value={user.gender}
-                style={{ marginBottom: "1rem" }}
-              />
-              <Select
-                label="Account Type"
-                placeholder="Select account type"
-                data={[
-                  { value: "individual", label: "Individual" },
-                  { value: "business", label: "Business" },
-                ]}
-                value={user.accounttype}
-                style={{ marginBottom: "1rem" }}
-              />
-            </SimpleGrid>
-
-            <Divider style={{ margin: "1rem 0" }} />
-
-            <SimpleGrid cols={1} breakpoints={[{ minWidth: "md", cols: 2 }]}>
-              <TextInput
-                type="email"
-                label="Email"
-                required
-                value={user.email}
-                style={{ marginBottom: "1rem" }}
-              />
-              <TextInput
-                label="Country"
-                required
-                value={user.location}
-                style={{ marginBottom: "1rem" }}
-              />
-            </SimpleGrid>
-
-            <Divider style={{ margin: "1rem 0" }} />
-
-            <SimpleGrid cols={1} breakpoints={[{ minWidth: "md", cols: 2 }]}>
-              <TextInput
-                label="Username"
-                description=" Use 3 to 15 characters, letters, numbers, and underscores only,
-            eg. ola_mike."
-                required
-                value={user.username}
-                style={{ marginBottom: "1rem" }}
-              />
-              <TextInput
-                label="Country"
-                description="We’re currently rolling out a few countries, visit our country roll-out plan for more into"
-                required
-                value={user.location}
-                style={{ marginBottom: "1rem" }}
-              />
-            </SimpleGrid>
-
-            <Divider style={{ margin: "1rem 0" }} />
-          </SimpleGrid>
-
-          <SimpleGrid cols={1}>
-            <Select
-              searchable
-              multiple
-              label="Select languages"
-              description="Use 3 to 15 characters, letters, numbers, and underscores only, eg. ola_mike"
-              placeholder="Select languages"
-              data={[
-                { value: "english", label: "English" },
-                { value: "spanish", label: "Spanish" },
-                { value: "french", label: "French" },
-                { value: "german", label: "German" },
-                { value: "italian", label: "Italian" },
-                { value: "japanese", label: "Japanese" },
-              ]}
-              value={languages}
-              onChange={handleLanguageChange}
-              filter={(value, item) =>
-                item.label.toLowerCase().includes(value.toLowerCase().trim())
-              }
-            />
-          </SimpleGrid>
-          <Divider style={{ margin: "1rem 0" }} />
-        </Paper>
       </PageContainer.Marketplace>
     </PageContainer>
   );
