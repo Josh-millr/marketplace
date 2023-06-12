f9dd36c8f1796a7d18c83ed3f9112f2c8fdf0878
Josh Millr <80153426+Josh-millr@users.noreply.github.com>
2023-06-12 04:04:34 +0000

    Testing

--- src/shared/components/Layout/Dashboard/Elements/NavItem/style.NavItem.js
+++ src/shared/components/Layout/Dashboard/Elements/NavItem/style.NavItem.js
@@ -1,7 +1,13 @@
 import { createStyles } from '@mantine/core';
 
-export const useStyles = createStyles(() => ({
+export const useStyles = createStyles((theme, { variant }) => ({
   wrapper: {
     width: '100%',
+  },
+
+  root: {
+    padding: `${theme.spacing.xl} ${theme.spacing.md}`,
+    backgroundColor:
+      variant === 'filled' ? theme.colors.blue[7] : 'transparent',
   },
 }));
