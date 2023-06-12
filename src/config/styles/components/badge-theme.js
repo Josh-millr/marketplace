719b444702113a09a2eb778392d9ee6658c5c0e3
Josh Millr <80153426+Josh-millr@users.noreply.github.com>
2023-06-12 15:15:56 +0000

    ad

--- src/config/styles/components/badge-theme.js
+++ src/config/styles/components/badge-theme.js
@@ -1,10 +1,24 @@
+/* eslint-disable prefer-destructuring */
 export const BadgeTheme = {
   styles: (theme, params, variant) => {
-    console.log('Variant', variant);
-    console.log('Params', params);
+    const root = {};
+
+    switch (variant) {
+      case 'filled':
+        root.backgroundColor = theme.colors.brandPrimary[8];
+        break;
+      case 'light':
+        root.backgroundColor = theme.colors.brandPrimary[2];
+        break;
+      default:
+        root.backgroundColor = 'transparent';
+        break;
+    }
 
     return {
-      root: {},
+      root: {
+        ...root,
+      },
 
       inner: {
         textTransform: 'uppercase',
