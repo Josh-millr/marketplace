87966ebd6a5ed9bd771c44e99b815267fae72a98
Josh Millr <80153426+Josh-millr@users.noreply.github.com>
2023-06-12 05:03:17 +0000

    as

--- src/shared/components/DashboardStatsCards/DashboardStatsCardBanner.jsx
+++ src/shared/components/DashboardStatsCards/DashboardStatsCardBanner.jsx
@@ -7,9 +7,8 @@ export function DashboardStatsCardBanner({ children }) {
       py={{ base: 'lg', sm: '2xl' }}
       breakpoints={[
         { maxWidth: 'xs', cols: 1 },
-        { maxWidth: 'md', cols: 2 },
         { maxWidth: 'lg', cols: 2 },
-        { maxWidth: 'xl', cols: 1 },
+        { maxWidth: 'xl', cols: 4 },
       ]}
     >
       {children}
