07d231c63c361eda62948a8e89ff9860c53179d9
Josh Millr <80153426+Josh-millr@users.noreply.github.com>
2023-06-12 04:41:36 +0000

    added mobile border

--- src/shared/components/DashboardSectionCardNew/style.SectionWrapperNew.js
+++ src/shared/components/DashboardSectionCardNew/style.SectionWrapperNew.js
@@ -8,6 +8,8 @@ export const useStyles = createStyles((theme) => ({
     padding: 0,
     width: '100%',
     height: 'fit-content',
+    borderTop: `1px solid ${theme.colors.neutral[4]}`,
+    borderBottom: `1px solid ${theme.colors.neutral[4]}`,
 
     [theme.fn.largerThan('md')]: {
       borderRadius: theme.radius.sm,
