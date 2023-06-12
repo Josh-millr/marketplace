631411c55a25a6de276be25e51aa9dd6ee26183f
Josh Millr <80153426+Josh-millr@users.noreply.github.com>
2023-06-12 04:25:04 +0000

    fexed paddin

--- src/config/styles/components/navlink-theme.js
+++ src/config/styles/components/navlink-theme.js
@@ -1,8 +1,7 @@
 export const NavLinkTheme = {
   styles: (theme) => ({
     root: {
-      padding: `${theme.spacing.xl} ${theme.spacing.md}`,
-      borderRadius: '0px 8px 8px 0px',
+      padding: `${theme.spacing.md} ${theme.spacing.xl}`,
     },
   }),
 };
