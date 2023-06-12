82a868608c51a2f97f5059d766df2e61f61e63d2
Josh Millr <80153426+Josh-millr@users.noreply.github.com>
2023-06-12 05:06:07 +0000

    remobe

--- src/shared/components/DashboardSectionCardNew/style.SectionWrapperNew.js
+++ src/shared/components/DashboardSectionCardNew/style.SectionWrapperNew.js
@@ -3,8 +3,6 @@ import { createStyles } from '@mantine/core';
 export const useStyles = createStyles((theme) => ({
   wrapper: {
     overflow: 'hidden',
-    marginTop: 24,
-    marginBottom: 24,
     padding: 0,
     width: '100%',
     height: 'fit-content',
