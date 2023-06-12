a45f36774067c3270385331f7b67e181af3188bf
Josh Millr <80153426+Josh-millr@users.noreply.github.com>
2023-06-12 04:47:54 +0000

    removed radi

--- src/shared/components/DashboardSectionCardNew/style.SectionWrapperNew.js
+++ src/shared/components/DashboardSectionCardNew/style.SectionWrapperNew.js
@@ -8,6 +8,7 @@ export const useStyles = createStyles((theme) => ({
     padding: 0,
     width: '100%',
     height: 'fit-content',
+    borderRadius: 0,
     borderTop: `1px solid ${theme.colors.neutral[4]}`,
     borderBottom: `1px solid ${theme.colors.neutral[4]}`,
 
