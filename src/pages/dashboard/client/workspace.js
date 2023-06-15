125cd71273da64db745b4f2a589cd96e47a8b296
Josh Millr <80153426+Josh-millr@users.noreply.github.com>
2023-06-15 03:36:04 -1200

    as

--- src/pages/dashboard/client/workspace.js
+++ src/pages/dashboard/client/workspace.js
@@ -1,9 +1,9 @@
-import { Tabs } from '@mantine/core';
+import { Tabs } from "@mantine/core";
 
-import { PageContainer } from '@/shared/components/PageContainer';
-import { SectionEmptyBanner } from '@/shared/components/SectionEmptyBanner';
-import { DashboardPageHeader } from '@/shared/components/DashboardPageHeader';
-import { DashboardSectionCardNew } from '@/shared/components/DashboardSectionCardNew';
+import { PageContainer } from "@/shared/components/PageContainer";
+import { SectionEmptyBanner } from "@/shared/components/SectionEmptyBanner";
+import { DashboardPageHeader } from "@/shared/components/DashboardPageHeader";
+import { DashboardSectionCardNew } from "@/shared/components/DashboardSectionCardNew";
 
 function Workspace() {
   return (
@@ -11,6 +11,25 @@ function Workspace() {
       <DashboardPageHeader title="My Workspace" />
 
       <DashboardSectionCardNew contentFullWidth>
+        <Tabs defaultValue="ongoing">
+          <Tabs.List>
+            <Tabs.Tab value="ongoing">Ongoing Jobs</Tabs.Tab>
+            <Tabs.Tab value="completed">Completed Jobs</Tabs.Tab>
+            <Tabs.Tab value="cancelled">Cancelled Jobs</Tabs.Tab>
+          </Tabs.List>
+
+          <Tabs.Panel value="ongoing" pt="xs">
+            Ongoing
+          </Tabs.Panel>
+
+          <Tabs.Panel value="completed" pt="xs">
+            Completed
+          </Tabs.Panel>
+
+          <Tabs.Panel value="cancelled" pt="xs">
+            Canceled
+          </Tabs.Panel>
+        </Tabs>
         {[].length === 0 && <SectionEmptyBanner sectionLabel="notifications" />}
       </DashboardSectionCardNew>
     </PageContainer.Marketplace>
