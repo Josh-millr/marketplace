d97a539090a510f74d87a0fdee8a0a861c88c0a0
Josh Millr <80153426+Josh-millr@users.noreply.github.com>
2023-06-15 00:09:32 +0000

    f

--- src/pages/dashboard/client/projects/project/[id].js
+++ src/pages/dashboard/client/projects/project/[id].js
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useDisclosure } from '@mantine/hooks';
 import {
   Flex,
